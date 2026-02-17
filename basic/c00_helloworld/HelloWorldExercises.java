b32bc4f65d874e426d22ec1f713c7db00313d6d8
Aaron-GF <aarongarcia567@gmail.com>
2026-02-17 13:01:28 +0100

    complete c00 exercises

--- basic/c00_helloworld/HelloWorldExercises.java
+++ basic/c00_helloworld/HelloWorldExercises.java
@@ -10,24 +10,41 @@ public class HelloWorldExercises {
     public static void main(String[] args) {
 
         // 1. Imprime un mensaje que diga tu nombre en lugar de "¡Hola Mundo!".
+        System.out.println("Aarón");
 
         // 2. Imprime dos líneas: "Hola" y luego "Mundo" con un solo println.
+        System.out.println("Hola\nMundo");
 
         // 3. Añade un comentario sobre lo que hace cada línea del programa.
 
         // 4. Crea un comentario en varias líneas.
+        /* 
+            Este es un comentario
+            de varias lineas
+         */
 
         // 5. Imprime tu edad, tu color favorito y tu ciudad.
+        System.out.println("Tengo 33 años, azul siempre en el corazón y mi ciudad es Noia");
 
-        // 6. Explora los diferentes System.XXX.println(); más allá de "out".
+        // 6. Explora los diferentes System.xxx.println(); más allá de "out".
+        System.err.println("El error es no intentarlo");
 
         // 7. Utiliza varios println para imprimir una frase.
+        System.out.println("Esto es");
+        System.out.println("una frase");
 
         // 8. Imprime un diseño ASCII (por ejemplo, una cara feliz usando símbolos).
+        System.out.println("  *****  ");
+        System.out.println(" *     * ");
+        System.out.println("* o   o *");
+        System.out.println("*   ^   *");
+        System.out.println("*    /  *");
+        System.out.println(" *  -  * ");
+        System.out.println("  *****  ");
 
         // 9. Intenta ejecutar el programa sin el método main y observa el error.
 
         //10. Intenta cambiar el nombre del archivo a uno diferente del de la clase y compílalo. ¿Qué pasa?
-
+        //-----> Si lo cambias en el archivo te pone el mismo nombre automaticamente en la clase y viceversa <------
     }
 }
