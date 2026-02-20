7126414565c1fd576b13b019b542ecca94fb264f
Aaron-GF <aarongarcia567@gmail.com>
2026-02-20 15:26:19 +0100

    Complete c04 exercises

--- basic/c04_conditionals/ConditionalsExercises.java
+++ basic/c04_conditionals/ConditionalsExercises.java
@@ -10,24 +10,111 @@ public class ConditionalsExercises {
     public static void main(String[] args) {
 
         // 1. Establece la edad de un usuario y muestra si puede votar (mayor o igual a 18).
+        int age = 33;
+        if (age >= 18) {
+            System.out.println("El usuario es mayor de edad y puede votar");
+        } else {
+            System.out.println("El usuario es menor de edad, no puede votar");
+        }
 
         // 2. Declara dos números y muestra cuál es mayor, o si son iguales.
+        int num1 = 3;
+        int num2 = 7;
+        if (num1 > num2) {
+            System.out.println("El número " + num1 + " es mayor que el número " + num2);
+        } else if (num2 > num1) {
+            System.out.println("El número " + num2 + " es mayor que el número " + num1);
+        } else {
+            System.out.println("Los números son iguales");
+        }
 
         // 3. Dado un número, verifica si es positivo, negativo o cero.
+        if (num2 > 0) {
+            System.out.println("El numero es positivo");
+        } else if (num2 < 0) {
+            System.out.println("El número es negativo");
+        } else {
+            System.out.println("El número es 0");
+        }
 
         // 4. Crea un programa que diga si un número es par o impar.
+        if (num1 % 2 == 0) {
+            System.out.println("El numero es par");
+        } else {
+            System.out.println("El número es impar");
+        }
 
         // 5. Verifica si un número está en el rango de 1 a 100.
+        int range = 100;
+        if (range > 0 && range < 101) {
+            System.out.println("El numero esta dentro del rango de 1 a 100");
+        } else {
+            System.out.println("El número esta fuera de rango");
+        }
 
         // 6. Declara una variable con el día de la semana (1-7) y muestra su nombre con switch.
+        var day = 1;
+        switch (day) {
+            case 1:
+                System.out.println("Lunes");
+                break;
+            case 2:
+                System.out.println("Martes");
+                break;
+            case 3:
+                System.out.println("Miercoles");
+                break;
+            case 4:
+                System.out.println("Jueves");
+                break;
+            case 5:
+                System.out.println("Viernes");
+                break;
+            case 6:
+                System.out.println("Sabado");
+                break;
+            case 7:
+                System.out.println("Domingo");
+                break;
+            default:
+                System.out.println("No es ningun día de la semana"); 
+        }
 
         // 7. Simula un sistema de notas: muestra "Sobresaliente", "Aprobado" o "Suspenso" según la nota (0-100).
+        int value = 75;
+        if (value < 50) {
+            System.out.println("Suspenso");
+        } else if (value < 80) {
+            System.out.println("Aprobado");
+        } else {
+            System.out.println("Sobresaliente");
+        }
 
         // 8. Escribe un programa que determine si puedes entrar al cine: debes tener al menos 15 años o ir acompañado.
+        if (age > 14) {
+            System.out.println("Puedes entrar al cine");
+        } else {
+            System.out.println("No puedes entrar si no es con un acompañante adulto");
+        }
 
         // 9. Crea un programa que diga si una letra es vocal o consonante.
+        char letter = 'g';
+        if ( letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
+            System.out.println("Es una vocal");
+        } else {
+            System.out.println("Es una consonante");
+        }
 
         // 10. Usa tres variables a, b, c y muestra cuál es el mayor de las tres.
-
+        int a = 20;
+        int b = 5;
+        int c = 14;
+        if (a > b && a > c) {
+            System.out.println("a: " + a);
+        } else if (b > a && b > c) {
+            System.err.println("b: " + b);
+        } else {
+            System.out.println("c: " + c);
+        }
     }
 }
